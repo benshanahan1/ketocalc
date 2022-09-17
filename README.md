6a5ed27eaf0fa69a9be9673e7d10c1642f8239d2
Benjamin Shanahan <benshanahan1@gmail.com>
2022-09-17 15:19:36 -0700

    Add protein selection and coconut oil target

---
+++ README.md
@@ -0,0 +1,7 @@
+# Keto Calculator
+
+Dog ketogenic diet calculator.
+
+Based off of the diet outlined at http://www.ketopetsanctuary.com.
+
+Hosted at https://benshanahan.dev/ketocalc
--- script.js
+++ script.js
@@ -1,12 +1,45 @@
 // Food targets in grams, from http://www.ketopetsanctuary.com
-var beefTargetGrams = 412.5;
-var broccoliTargetGrams = 99.5;
+var targets = {
+  beef: {
+    proteinTargetGrams: 412.5,
+    vegetableTargetGrams: 99.5,
+    oilTargetGrams: 0,
+  },
+  chicken: {
+    proteinTargetGrams: 304.9,
+    vegetableTargetGrams: 99.5,
+    oilTargetGrams: 62.5,
+  },
+  turkey: {
+    proteinTargetGrams: 325.7,
+    vegetableTargetGrams: 87.8,
+    oilTargetGrams: 40.1,
+  },
+};
 
 // Constants
 var poundsToGrams = 453.592;
 var precision = 2; // how many decimals to display on values
 
 // Helper functions
+function getElements() {
+  return {
+    // Sections
+    oilSection: document.getElementById("oilSection"),
+
+    // Selects
+    proteinSelect: document.getElementById("proteinSelect"),
+
+    // Inputs
+    proteinPoundsInput: document.getElementById("proteinPounds"),
+    proteinGramsInput: document.getElementById("proteinGrams"),
+    vegetablePoundsInput: document.getElementById("vegetablePounds"),
+    vegetableGramsInput: document.getElementById("vegetableGrams"),
+    oilGramsInput: document.getElementById("oilGrams"),
+    daysInput: document.getElementById("days"),
+  };
+}
+
 function toGrams(value) {
   return value * poundsToGrams;
 }
@@ -23,83 +56,137 @@ function setText(inputElement, number) {
   inputElement.value = number.toFixed(precision);
 }
 
-function calculate(inputElementId) {
-  // Get all input elements.
-  var beefPoundsInput = document.getElementById("beefPounds");
-  var beefGramsInput = document.getElementById("beefGrams");
-  var broccoliPoundsInput = document.getElementById("broccoliPounds");
-  var broccoliGramsInput = document.getElementById("broccoliGrams");
-  var daysInput = document.getElementById("days");
+function getTargetsBySelectedProtein() {
+  return targets[getElements().proteinSelect.value];
+}
 
-  var beefPounds = parseFloat(beefPoundsInput.value);
-  var beefGrams = parseFloat(beefGramsInput.value);
-  var broccoliPounds = parseFloat(broccoliPoundsInput.value);
-  var broccoliGrams = parseFloat(broccoliGramsInput.value);
+function updateInterface() {
+  var targets = getTargetsBySelectedProtein();
+  var elements = getElements();
+  if (targets.oilTargetGrams <= 0) {
+    elements.oilSection.classList.remove("show");
+    elements.oilSection.classList.add("hide");
+  } else {
+    elements.oilSection.classList.remove("hide");
+    elements.oilSection.classList.add("show");
+  }
+
+  // If protein input has a value, recompute all fields from it.
+  if (!isNaN(parseFloat(elements.proteinPoundsInput.value))) {
+    calculate(elements.proteinPoundsInput.id);
+  }
+}
+
+function calculate(inputElementId) {
+  // Get targets
+  var { proteinTargetGrams, vegetableTargetGrams, oilTargetGrams } =
+    getTargetsBySelectedProtein();
+
+  // Get all input elements.
+  var {
+    proteinPoundsInput,
+    proteinGramsInput,
+    proteinGramsInput,
+    vegetablePoundsInput,
+    vegetableGramsInput,
+    oilGramsInput,
+    daysInput,
+  } = getElements();
+
+  var proteinPounds = parseFloat(proteinPoundsInput.value);
+  var proteinGrams = parseFloat(proteinGramsInput.value);
+  var vegetablePounds = parseFloat(vegetablePoundsInput.value);
+  var vegetableGrams = parseFloat(vegetableGramsInput.value);
+  var oilGrams = parseFloat(oilGramsInput.value);
   var days = parseFloat(daysInput.value);
 
   // Validate inputs.
-  if (isNaN(beefPounds)) beefPoundsInput.value = null;
-  if (isNaN(beefGrams)) beefGramsInput.value = null;
-  if (isNaN(broccoliPounds)) broccoliPoundsInput.value = null;
-  if (isNaN(broccoliGrams)) broccoliGramsInput.value = null;
+  if (isNaN(proteinPounds)) proteinPoundsInput.value = null;
+  if (isNaN(proteinGrams)) proteinGramsInput.value = null;
+  if (isNaN(vegetablePounds)) vegetablePoundsInput.value = null;
+  if (isNaN(vegetableGrams)) vegetableGramsInput.value = null;
+  if (isNaN(oilGrams)) oilGramsInput.value = null;
   if (isNaN(days)) daysInput.value = null;
 
   // Populate values based on input element id
   switch (inputElementId) {
-    case "beefPounds":
-      beefGrams = toGrams(beefPounds);
-      days = computeDaysOfFood(beefGrams, beefTargetGrams);
-      broccoliGrams = days * broccoliTargetGrams;
-      broccoliPounds = toPounds(broccoliGrams);
+    case "proteinPounds":
+      proteinGrams = toGrams(proteinPounds);
+      days = computeDaysOfFood(proteinGrams, proteinTargetGrams);
+      vegetableGrams = days * vegetableTargetGrams;
+      vegetablePounds = toPounds(vegetableGrams);
+      oilGrams = days * oilTargetGrams;
       // Update inputs
-      setText(beefGramsInput, beefGrams);
-      setText(broccoliGramsInput, broccoliGrams);
-      setText(broccoliPoundsInput, broccoliPounds);
+      setText(proteinGramsInput, proteinGrams);
+      setText(vegetableGramsInput, vegetableGrams);
+      setText(vegetablePoundsInput, vegetablePounds);
+      setText(oilGramsInput, oilGrams);
       setText(daysInput, days);
       break;
-    case "beefGrams":
-      days = computeDaysOfFood(beefGrams, beefTargetGrams);
-      beefPounds = toPounds(beefGrams);
-      broccoliGrams = days * broccoliTargetGrams;
-      broccoliPounds = toPounds(broccoliGrams);
+    case "proteinGrams":
+      days = computeDaysOfFood(proteinGrams, proteinTargetGrams);
+      proteinPounds = toPounds(proteinGrams);
+      vegetableGrams = days * vegetableTargetGrams;
+      vegetablePounds = toPounds(vegetableGrams);
+      oilGrams = days * oilTargetGrams;
       // Update inputs
-      setText(beefPoundsInput, beefPounds);
-      setText(broccoliGramsInput, broccoliGrams);
-      setText(broccoliPoundsInput, broccoliPounds);
+      setText(proteinPoundsInput, proteinPounds);
+      setText(vegetableGramsInput, vegetableGrams);
+      setText(vegetablePoundsInput, vegetablePounds);
+      setText(oilGramsInput, oilGrams);
       setText(daysInput, days);
       break;
-    case "broccoliPounds":
-      broccoliGrams = toGrams(broccoliPounds);
-      days = computeDaysOfFood(broccoliGrams, broccoliTargetGrams);
-      beefGrams = days * beefTargetGrams;
-      beefPounds = toPounds(beefGrams);
+    case "vegetablePounds":
+      vegetableGrams = toGrams(vegetablePounds);
+      days = computeDaysOfFood(vegetableGrams, vegetableTargetGrams);
+      proteinGrams = days * proteinTargetGrams;
+      proteinPounds = toPounds(proteinGrams);
+      oilGrams = days * oilTargetGrams;
       // Update inputs
-      setText(beefGramsInput, beefGrams);
-      setText(beefPoundsInput, beefPounds);
-      setText(broccoliGramsInput, broccoliGrams);
+      setText(proteinGramsInput, proteinGrams);
+      setText(proteinPoundsInput, proteinPounds);
+      setText(vegetableGramsInput, vegetableGrams);
+      setText(oilGramsInput, oilGrams);
       setText(daysInput, days);
       break;
-    case "broccoliGrams":
-      days = computeDaysOfFood(broccoliGrams, broccoliTargetGrams);
-      broccoliPounds = toPounds(broccoliGrams);
-      beefGrams = days * beefTargetGrams;
-      beefPounds = toPounds(beefGrams);
+    case "vegetableGrams":
+      days = computeDaysOfFood(vegetableGrams, vegetableTargetGrams);
+      vegetablePounds = toPounds(vegetableGrams);
+      proteinGrams = days * proteinTargetGrams;
+      proteinPounds = toPounds(proteinGrams);
+      oilGrams = days * oilTargetGrams;
       // Update inputs
-      setText(beefGramsInput, beefGrams);
-      setText(beefPoundsInput, beefPounds);
-      setText(broccoliPoundsInput, broccoliPounds);
+      setText(proteinGramsInput, proteinGrams);
+      setText(proteinPoundsInput, proteinPounds);
+      setText(vegetablePoundsInput, vegetablePounds);
+      setText(oilGramsInput, oilGrams);
+      setText(daysInput, days);
+      break;
+    case "oilGrams":
+      days = computeDaysOfFood(oilGrams, oilTargetGrams);
+      vegetableGrams = days * vegetableTargetGrams;
+      vegetablePounds = toPounds(vegetableGrams);
+      proteinGrams = days * proteinTargetGrams;
+      proteinPounds = toPounds(proteinGrams);
+      // Update inputs
+      setText(proteinGramsInput, proteinGrams);
+      setText(proteinPoundsInput, proteinPounds);
+      setText(vegetableGramsInput, vegetableGrams);
+      setText(vegetablePoundsInput, vegetablePounds);
       setText(daysInput, days);
       break;
     case "days":
-      beefGrams = days * beefTargetGrams;
-      beefPounds = toPounds(beefGrams);
-      broccoliGrams = days * broccoliTargetGrams;
-      broccoliPounds = toPounds(broccoliGrams);
+      proteinGrams = days * proteinTargetGrams;
+      proteinPounds = toPounds(proteinGrams);
+      vegetableGrams = days * vegetableTargetGrams;
+      vegetablePounds = toPounds(vegetableGrams);
+      oilGrams = days * oilTargetGrams;
       // Update inputs
-      setText(beefGramsInput, beefGrams);
-      setText(beefPoundsInput, beefPounds);
-      setText(broccoliGramsInput, broccoliGrams);
-      setText(broccoliPoundsInput, broccoliPounds);
+      setText(proteinGramsInput, proteinGrams);
+      setText(proteinPoundsInput, proteinPounds);
+      setText(vegetableGramsInput, vegetableGrams);
+      setText(vegetablePoundsInput, vegetablePounds);
+      setText(oilGramsInput, oilGrams);
       break;
     default:
       console.log("Unrecognized input element id.");
